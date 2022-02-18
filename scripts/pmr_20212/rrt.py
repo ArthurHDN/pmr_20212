0e13361671f5214172d5ab5049d9b080c1f50961
Álvaro Rodrigues Araújo <52823987+ara1557@users.noreply.github.com>
2022-02-18 16:13:48 -0300

    Some changes in rrt_star classes

--- scripts/pmr_20212/rrt.py
+++ scripts/pmr_20212/rrt.py
@@ -247,38 +247,3 @@ class RRT(object):
         d = math.hypot(dx, dy)
         theta = math.atan2(dy, dx)
         return d, theta
-
-"""
-def main(gx=6.0, gy=10.0):
-    print("start " + __file__)
-
-    # ====Search Path with RRT====
-    obstacleList = [(5, 5, 1), (3, 6, 2), (3, 8, 2), (3, 10, 2), (7, 5, 2),
-                    (9, 5, 2), (8, 10, 1)]  # [x, y, radius]
-    # Set Initial parameters
-    rrt = RRT(
-        start=[0, 0],
-        goal=[gx, gy],
-        rand_area=[-2, 15],
-        obstacle_list=obstacleList,
-        # play_area=[0, 10, 0, 14]
-        )
-    path = rrt.planning(animation=show_animation)
-
-    if path is None:
-        print("Cannot find path")
-    else:
-        print("found path!!")
-
-        # Draw final path
-        if show_animation:
-            rrt.draw_graph()
-            plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
-            plt.grid(True)
-            plt.pause(0.01)  # Need for Mac
-            plt.show()
-
-
-if __name__ == '__main__':
-    main()
-"""
